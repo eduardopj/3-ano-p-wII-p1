6696ba97e810ebbac2e00210d7e7e57aa3d0f50b
Eduardo Pinheiro Júnior <37462997+eduardopj@users.noreply.github.com>
2025-09-05 10:49:28 -0500

    Update conexao.php
    Atualização

--- 3B/aula-01/conexao.php
+++ 3B/aula-01/conexao.php
@@ -2,7 +2,7 @@
 $servidor = "localhost";
 $banco = "ifac";
 $usuario = "root";
-$senha = ""
+$senha = "";
 $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
 if(!$conexao){
     die("erro ao conectar".mysqli_connect_error());
