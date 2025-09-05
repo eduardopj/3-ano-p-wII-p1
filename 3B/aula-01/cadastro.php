f73b80a31677dbb632d884b69b1d0204eef449f4
Eduardo Pinheiro Júnior <37462997+eduardopj@users.noreply.github.com>
2025-09-05 10:06:43 -0500

    Atualização

--- 3B/aula-01/cadastro.php
+++ 3B/aula-01/cadastro.php
@@ -7,22 +7,31 @@
     <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
+    <nav>
+        <ul>
+            <li><a href="#">Início</a></li>
+            <li><a href="cadastro.html" class="active">Cadastrar Aluno</a></li>
+            <li><a href="lista.html">Lista de Alunos</a></li>
+            <li><a href="#">Sobre</a></li>
+        </ul>
+    </nav>
     <h1>Cadastro de Aluno</h1>
     <hr>
     <form method="post">
         <label for="nome">Nome</label>
         <input type="text" name="nome" id="nome" class="texttext">
         <label for="email">Email</label>
-        <input type="email" name="email" id="email" class="texttext" >
+        <input type="email" name="email" id="email" class="texttext">
         <label for="cpf">CPF</label>
-        <input type="text" name="cpf" id="cpf" class="texttext" >
+        <input type="text" name="cpf" id="cpf" class="texttext">
         <label for="dtNasc">Data de Nascimento</label>
         <input type="text" name="dtNasc" id="dtNasc" class="texttext">
         <label for="mae">Mãe</label>
         <input type="text" name="mae" id="mae" class="texttext">
-        <button type="submit">Cadastrar</button>
-
-
+        <div class="botoes">
+            <button type="submit">Cadastrar</button>
+            <button type="reset" class="limpar">Limpar</button>
+        </div>
     </form>
 </body>
 </html>
